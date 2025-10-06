9e29a62d94c407ce9d0f0506d73d60a470f12a78
norton582 <norton.rakotovao@gmail.com>
2025-10-06 18:33:14 +0300

    Update eslint-config pour la production

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
     prisma.post.count()
   ])
   
-  const CountPost = 0;
+
 
   return (
     <div className="container mx-auto px-4 py-8 max-w-4xl">
--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,6 +1,5 @@
 import { SubmitButton } from "@/components/submitButton";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { prisma } from "@/lib/prisma";
